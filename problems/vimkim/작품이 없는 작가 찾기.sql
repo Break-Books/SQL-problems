62c8ca7c25c6f79276627205b294512706cf5d56
Daehyun Kim <18080546+vimkim@users.noreply.github.com>
2024-02-25 12:01:32 +0900

    [1주차] solvedSQL_김대현 (#3)

---
+++ problems/vimkim/작품이 없는 작가 찾기.sql
@@ -0,0 +1,14 @@
+-- ok
+SELECT
+    a.artist_id,
+    a.name
+FROM
+    artists a
+    LEFT JOIN artworks_artists aa ON a.artist_id = aa.artist_id
+WHERE
+    aa.artwork_id IS NULL
+    AND a.death_year IS NOT NULL
+GROUP BY
+    a.artist_id,
+    a.name
+;
